fea62dc1b51caf908eff6ba16072ddb83a200f14
nahnn <nathanrezende30@gmail.com>
2025-06-14 14:47:29 -0400

    feat: HuffmanTree, refactor: MinHeap

--- MinHeap/MinHeap.cpp
+++ MinHeap/MinHeap.cpp
@@ -16,14 +16,14 @@ public:
     Node();
     Node(int key, char simb);
     ~Node();
-    void print(char sep);
+    void print(const char *sep = " ");
 
 private:
     int key;
     char simb;
-    Node* lChild;
-    Node* rChild;
     Node* parent;
+    Node* left;
+    Node* right;
 };
 
 class Heap
@@ -58,92 +58,33 @@ int main(void)
   return 0;
 }
 
-/*
-
-Heap h2(5, v);  // o construtor é usado para construir h2
-Heap h3(h2);    // o construtor de cópia é usado para construir h3
-Heap h4 = h3;   // o construtor de cópia é usado para inicializar um objeto quando há uma atribuição SIMULTÂNEA com a declaração
-h = h2;         // o operador de atribuição (cópia) é utilizado (não algum construtor ou transferência)
-h = Heap(5, v); // o construtor é usado para construir uma variável temporária, e depois o operação de atribuição (cópia?) é utilizado
-
-** Quando são feitas cópias de objetos **
-
-O C++ chama um construtor de cópia para fazer uma cópia de um objeto
-conforme descrito nos casos acima. Se não houver um construtor de
-cópias definido para a classe, o C ++ definirá um construtor de cópias
-padrão, que copia cada campo (utilizando o construtor de cópia de cada
-campo, se for o caso).
-
-
-** Não escreva um construtor de cópias a menos que necessário **
-
-Se o objeto não tiver ponteiros para a memória alocada dinâmicamente,
-uma cópia "superficial" provavelmente será suficiente. Portanto, o
-construtor de cópia padrão, o operador de atribuição padrão e o
-destruidor padrão estão ok e você não precisa escrever seus próprios.
-
-
-** Se você precisar de um construtor de cópias, também precisará de um **
-** destruidor e um operador de atribuição (operator=) **
-
-Se você precisa de um construtor de cópias, é porque precisa de algo
-como uma cópia profunda ou algum outro gerenciamento de
-recursos. Assim, é quase certo que você precisará definir um destruidor e
-um operador de atribuição.
-
-
-** Diferença entre construtor de cópia e atribuição **
-
-Um construtor de cópia é usado para inicializar uma variável EM SUA
-DECLARAÇÃO a partir de uma variável existente. É parecido com a
-atribuição, mas mais simples:
-
-1. Não há necessidade de testar para ver se ele está sendo inicializado por si mesmo
-2. Não há necessidade de limpar (por exemplo, excluir) um valor existente (não há nenhum)
-3. Uma referência a si mesma não é devolvida.
-
-
-** Transferência **
-
-Na maioria das vezes, o construtor de transferência ou o operador de
-atribuição de trasnferência não são fornecidos pelo compilador. Se
-forem, eles farão o mesmo que os construtores/operadores de cópia.
-
-Regra: se você deseja um construtor de transferência e uma atribuição
-de transferência que realmente realize a transferência de dados, você
-terá que escrevê-los
-
-*/
-
 //*************************************
 //*** IMPLEMENTAÇÕES DA CLASSE NODE ***
 //*************************************
-Node::Node()
-{
-    key = 0;
-    simb = '\0';
-    lChild = nullptr;
-    rChild = nullptr;
-    parent = nullptr;
-}
+Node::Node() :
+  key(0),
+  simb('\0'),
+  parent(nullptr),
+  left(nullptr),
+  right(nullptr)
+  {}
 
-Node::Node(int key, char simb)
-{
-    this->key = key;
-    this->simb = simb;
-    lChild = nullptr;
-    rChild = nullptr;
-    parent = nullptr;
-}
+Node::Node(int key, char simb) :
+  key(key),
+  simb(simb),
+  parent(nullptr),
+  left(nullptr),
+  right(nullptr)
+  {}
 
 Node::~Node()
 {
     delete this;
 }
 
-void Node::print(char sep)
+void Node::print(const char *sep)
 {
-    printf("%d%c", this->key, sep);
+    printf("%d%s", key, sep);
 }
 
 //*************************************
@@ -174,7 +115,7 @@ void Heap::escreve_niveis()
 
     for(int i = 0; i < sLength; i++) {
         // Equivalente a for (unsigned i = 0; i < S.size(); i++) { printf("%d ", S[i]);
-        S[i]->print(' ');
+        S[i]->print();
         if (++escritos == fim_nivel) {
             putchar('\n');
             fim_nivel *= 2;
@@ -193,7 +134,7 @@ void Heap::escreve(const string& prefixo, int i)
     printf(prefixo.c_str());
     printf(ehEsquerdo and temIrmao ? "├──" : "└──" );
 
-    S[i]->print('\n');
+    S[i]->print("\n");
 
     escreve(prefixo + (ehEsquerdo ? "│   " : "    "), esquerdo(i));
     escreve(prefixo + (ehEsquerdo ? "│   " : "    "), direito(i));
@@ -253,12 +194,12 @@ void Heap::insere(Node* p)
     sobe(S.size()-1);
 }
 
-Node* Heap::consulta_maxima()
+Node* Heap::consulta_minima()
 {
     return S[0];
 }
 
-Node* Heap::extrai_maxima()
+Node* Heap::extrai_minima()
 {
     Node* menor;
     if (S.size() > 0) {
